91639f322d163566c132cac09652364ed2a37ca9
Tahsin Emre Telli <telli@tahsinemre.com>
2025-04-18 15:46:24 +0300

    Update dependencies and integrate flutter_3d_controller and flutter_inappwebview packages
    - Added flutter_3d_controller as a direct dependency in pubspec.yaml for enhanced 3D model viewing capabilities.
    - Included multiple flutter_inappwebview packages in pubspec.lock to support in-app web views across different platforms.
    - Updated iOS Podfile.lock to reflect new dependencies and ensure compatibility.
    - Modified ModelViewerView to utilize flutter_3d_controller for improved model loading and error handling.
    - Adjusted saved scans view to pass the file parameter to ModelViewerView for better file management.

--- lib/feature/model_viewer/view/model_viewer_view.dart
+++ lib/feature/model_viewer/view/model_viewer_view.dart
@@ -1,32 +1,48 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_3d_controller/flutter_3d_controller.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class ModelViewerView extends StatelessWidget {
-  const ModelViewerView({super.key, required this.modelPath});
-
+  const ModelViewerView({
+    required this.modelPath,
+    required this.file,
+    super.key,
+  });
+  final FileSystemEntity file;
   final String modelPath;
 
   @override
   Widget build(BuildContext context) {
     // Extract filename for the AppBar title
     final fileName = modelPath.split('/').last;
-
+    final file = File(modelPath);
+    print(file);
     return Scaffold(
       appBar: AppBar(title: Text(fileName)),
-      body: ModelViewer(
-        src: 'file://$modelPath', // Important: Use the file:// scheme
-        alt: "A 3D model of $fileName",
-        ar: true, // Enable AR Quick Look on iOS
-        autoRotate: true,
-        cameraControls: true,
-        // You might need to adjust background color depending on model
-        backgroundColor: Colors.white,
-        // Optional: Add error handling
-        loading: Loading.eager,
-        // Optional: Specify camera settings
-        // cameraOrbit: '0deg 75deg 1.5m',
-        // cameraTarget: '0m 0m 0m',
+      body: Flutter3DViewer.obj(
+        src: file.path,
+
+        onProgress: (double progressValue) {
+          debugPrint('model loading progress : $progressValue');
+        },
+        //This callBack will call after model loaded successfully and will return model address
+        onLoad: (String modelAddress) {
+          debugPrint('model loaded : $modelAddress');
+        },
+        //this callBack will call when model failed to load and will return failure erro
+        onError: (String error) {
+          debugPrint('model failed to load : $error');
+        },
       ),
     );
   }
 }
+
+
+
+  // body: ModelViewer(
+  //       src: 'file://$modelPath', // Important: Use the file:// scheme
+
+  //     ),
